31f0c118b7e5b84301ac3a469164cd2ee5ae749c
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-09-10 21:07:54 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] freq=new int[26];
+        if(s.length()!=t.length())  return false;
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            freq[ch-'a']--;
+
+            if(freq[ch-'a']<0)
+                return false;
+        }
+
+        
+        for(int count:freq){
+            if(count==0){
+                return true;
+            }
+        }
+        return false;
+    }
+}
